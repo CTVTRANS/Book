04e23e01d077878060a259216331295a114728d9
lekienhust94 <lekienhust94@gmail.com>
2017-10-25 17:33:18 +0700

    add libs payments

---
+++ BookApp/LibrariesPayment-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  LibrariesPayment-Bridging-Header.h
+//  testPayment
+//
+//  Created by kien le van on 10/25/17.
+//  Copyright © 2017 kien le van. All rights reserved.
+//
+
+#ifndef LibrariesPayment_Bridging_Header_h
+#define LibrariesPayment_Bridging_Header_h
+
+#import "FuqianlaPay.h"
+#import "WXApi.h"
+#import <CommonCrypto/CommonCrypto.h>
+
+
+#endif /* LibrariesPayment_Bridging_Header_h */
